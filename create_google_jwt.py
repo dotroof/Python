779fc7e48884b060d3b0e9757d92a110410a7dd7
dotroof <47698090+dotroof@users.noreply.github.com>
2021-09-26 15:12:13 +0900

    GoogleAPIを使う時のJWTの生成

---
+++ create_google_jwt.py
@@ -0,0 +1,26 @@
+import json
+import time
+
+import jwt
+
+service_json_dict = open("./service.json", "r")
+service_json = json.load(service_json_dict)
+
+headers = {
+    "alg": "RS256",
+    "typ": "JWT"
+}
+
+iat = time.time()
+exp = iat + 3600
+payload = {
+    "iss": service_json["client_email"],
+    "scope": "https://www.googleapis.com/auth/androidpublisher",
+    "aud": "https://oauth2.googleapis.com/token",
+    "exp": exp,
+    "iat": iat
+}
+
+token = jwt.encode(payload, service_json["private_key"], algorithm="RS256", headers=headers)
+
+print("jwt: ", token)
